37f7803be5d3c553f1cf5981d678ec2cc0cda6fa
attmhd <2022610013.attan@itp.ac.id>
2024-02-25 22:35:30 +0700

    feat: Entry Data Admin && test: Check DB Connection

---
+++ admingd/entity.go
@@ -0,0 +1,9 @@
+package admingd
+
+type Admgd struct {
+	Id       int
+	Name     string
+	Username string
+	Password string
+	Email    string
+}
--- main.go
+++ main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"inventory-manajemen-system/admingd"
 	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type User struct {
-	gorm.Model
-	Name  string
-	Email string
-}
-
 func main() {
 
 	dsn := "host=localhost user=postgres password=postgres dbname=inventory port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	// fmt.Println("Koneksi sukses")
+	tstRepo := admingd.NewRepository(db)
+	user := admingd.Admgd{
+		Id:   1,
+		Name: "Testing dlu",
+	}
+
+	tstRepo.Save(user)
+
+	//fmt.Println("Koneksi sukses")
+
+	// r := gin.Default()
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run() // listen and serve on 0.0.0.0:8080
 }
